d7fc02e55b9ff1c3ae20902e1c27127c8b352cab
leng_glb <leng_glb@qq.com>
2018-12-20 17:54:29 +0800

    sync

---
+++ src/main/java/com/expolre/code/CodeTest.java
@@ -0,0 +1,84 @@
+package com.expolre.code;
+
+import static org.junit.Assert.fail;
+
+import java.util.Queue;
+import java.util.concurrent.ConcurrentLinkedQueue;
+
+import org.junit.Test;
+
+public class CodeTest {
+
+	
+	public static void main(String[] args) {
+		
+		
+		Queue<String> queue = new ConcurrentLinkedQueue<>();
+		for (int i = 0; i < 1000; i++) {
+			queue.offer("a"+i);
+		}
+		System.out.println("-----------------------");
+		
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (!queue.isEmpty()) {
+					String str = queue.poll();
+					System.out.println(Thread.currentThread().getName()+":"+str);
+				}	
+			}
+		}).start();
+
+		new Thread(new Runnable() {	
+
+			@Override
+			public void run() {
+				while (!queue.isEmpty()) {
+					String str = queue.poll();
+					System.out.println(Thread.currentThread().getName()+":"+str);
+				}
+			}
+		}).start();
+
+		new Thread(new Runnable() {
+
+			@Override
+			public void run() {
+				while (!queue.isEmpty()) {
+					String str = queue.poll();
+					System.out.println(Thread.currentThread().getName()+":"+str);
+				}
+			}
+		}).start();
+		
+		
+		/*new Thread(() -> {
+			for (int i = 0; i < queue.size(); i++) {				
+				String str = queue.poll();
+				System.out.println(str);
+			}
+		}).start();
+		
+		new Thread(() -> {
+			for (int i = 0; i < queue.size(); i++) {				
+				String str = queue.poll();
+				System.out.println(str);
+			}
+		}).start();
+		
+		new Thread(() -> {
+			for (int i = 0; i < queue.size(); i++) {				
+				String str = queue.poll();
+				System.out.println(str);
+			}
+		}).start();*/
+		
+	}
+	
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
---
+++ src/main/java/com/expolre/thread/sync/SynchronizedTest.java
@@ -0,0 +1,29 @@
+package com.expolre.thread.sync;
+
+/**
+ * Java语言关键字-->synchronized(同步锁)
+ * @author lengyul
+ * @date 2018年12月20日 下午3:31:18
+ * 保证原子性和可见性
+ * 意味着同一时刻保证只有一个线程去执行被synchronized锁定的代码。
+ * 两种用法：
+ * 	1.对象锁：方法（锁对象默认为this）和同步代码块
+ *  2.类锁：静态方法和锁Class对象
+ */
+public class SynchronizedTest {
+	//对象锁
+	public synchronized void test1(){} //默认为this
+	public void test2(){
+		synchronized (this) { //手动指定锁对象
+		}
+	}
+	
+	//类锁
+	public static synchronized void test3(){}
+	public void test4(){
+		synchronized (SynchronizedTest.class) {
+		}
+	}
+	
+
+}
