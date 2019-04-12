646ed6ed31adc7a8af58fdefe5115bf706cbe2c8
lengyul <lengyul@192.168.8.108>
2019-04-12 11:36:42 +0800

    modify functions

---
+++ src/main/java/pers/allen/explore/code/java8/functions/CalcFunction.java
@@ -0,0 +1,15 @@
+package pers.allen.explore.code.java8.functions;
+
+@FunctionalInterface
+public interface CalcFunction {
+	
+	long getValue(int x, int y);
+	
+	public static void main(String[] args) {
+		CalcFunction calcPlus = (x,y) -> x + y;
+		System.out.println(calcPlus.getValue(1, 2));
+		
+		CalcFunction calcMmultiply = (x,y) -> x * y;
+		System.out.println(calcMmultiply.getValue(1, 2));
+	}
+}
