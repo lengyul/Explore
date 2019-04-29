7ebee448b4402a9bff2cfb08a96e09f8aa466c0b
lengyul <lengyul@192.168.8.112>
2019-04-29 17:24:24 +0800

    codes format

--- src/main/java/pers/allen/explore/thread/pool/WorkQueue.java
+++ src/main/java/pers/allen/explore/thread/pool/WorkQueue.java
@@ -4,40 +4,41 @@ import java.util.LinkedList;
 
 /**
  * 具有线程池的工作队列
+ * 
  * @author lengyul
  * @date 2018年12月21日 上午9:37:50
  */
 public class WorkQueue {
-	
+
 	@SuppressWarnings("unused")
-	private final int nThreads; //线程池大小
-	private LinkedList<Runnable> queue; //保存要执行线程的队列
-	private PoolWorker[] poolWorkers; //工作线程数组
-	
-	public WorkQueue(int nThreads){
+	private final int nThreads; // 线程池大小
+	private LinkedList<Runnable> queue; // 保存要执行线程的队列
+	private PoolWorker[] poolWorkers; // 工作线程数组
+
+	public WorkQueue(int nThreads) {
 		this.nThreads = nThreads;
 		queue = new LinkedList<>();
 		poolWorkers = new PoolWorker[nThreads];
 		for (int i = 0; i < nThreads; i++) {
 			poolWorkers[i] = new PoolWorker();
-			poolWorkers[i].start();//启动工作线程
+			poolWorkers[i].start();// 启动工作线程
 		}
 	}
-	
-	public void execute(Runnable r){
+
+	public void execute(Runnable r) {
 		synchronized (queue) {
 			queue.addLast(r);
-			queue.notify(); //唤醒正在等待的工作线程，建议使用 notifyAll() 
+			queue.notify(); // 唤醒正在等待的工作线程，建议使用 notifyAll()
 		}
 	}
-	
-	private class PoolWorker extends Thread{
+
+	private class PoolWorker extends Thread {
 		@Override
-		public void run(){
+		public void run() {
 			Runnable r = null;
 			while (true) {
 				synchronized (queue) {
-					while (queue.isEmpty()) { //避免假唤醒
+					while (queue.isEmpty()) { // 避免假唤醒
 						try {
 							queue.wait();
 						} catch (InterruptedException e) {
@@ -46,8 +47,8 @@ public class WorkQueue {
 					}
 					r = queue.removeFirst();
 				}
-				r.run(); //执行完毕后再次轮询
+				r.run(); // 执行完毕后再次轮询
 			}
 		}
-	} 
+	}
 }
