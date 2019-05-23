16948e6bba41539f090a080d5901bc8dcabd5eba
lengyul <1174959274@qq.com>
2019-05-23 16:14:45 +0800

    add note

--- src/main/java/pers/allen/explore/thread/sync/SynchronizedTest.java
+++ src/main/java/pers/allen/explore/thread/sync/SynchronizedTest.java
@@ -4,7 +4,7 @@ import java.time.Instant;
 import java.util.concurrent.TimeUnit;
 
 /**
- * Java语言关键字-->synchronized(同步锁)
+ * Java语言关键字-->synchronized(内置锁、同步锁)
  * @author lengyul
  * @date 2018年12月20日 下午3:31:18
  * 保证原子性(monitorenter / monitorexit)和可见性(释放锁之前更新主内存，获取锁时重新去读取主存数据)
@@ -13,6 +13,8 @@ import java.util.concurrent.TimeUnit;
  * 两种用法：
  * 	1.对象锁：方法（锁对象默认为this）和同步代码块
  *  2.类锁：静态方法和锁Class对象
+ *  
+ *  笔记：{@link #https://blog.csdn.net/qq_34808893/article/details/90316908}
  */
 public class SynchronizedTest {
 	//对象锁
