aba56bdb86e1e213dfa65dadc75e8ecda5a1cec2
lengyul <1174959274@qq.com>
2019-05-30 10:22:46 +0800

    stream

---
+++ src/main/java/pers/allen/explore/feature/java8/stream/LazyCalculation.java
@@ -0,0 +1,31 @@
+package pers.allen.explore.feature.java8.stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Stream;
+
+/**
+ * 惰性计算：意味着一个函数可以现在或以后计算，或者可以完全跳过计算
+ * @author lengyul
+ * @date 2019年5月29日 上午11:25:14
+ * 
+ * 传递给操作的 lambda 表达式和闭包应该是纯的（不可变），从函数管道的开始到结束，闭包所依赖的状态绝不应被多个线程修改
+ * 函数纯度的规则：
+ * 	函数不会更改任何元素
+ *  函数不依赖于任何可能改变的元素
+ */
+public class LazyCalculation {
+	
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1, 2, 3);
+		
+		int[] factor = new int[] { 2 };
+		Stream<Integer> stream = numbers.stream().map(e -> e * factor[0]);
+		/*
+		 * map 为中间操作，forEach 为终结操作
+		 * map 的闭包依赖的变量可能发生变化，由于惰性计算，作为参数传递给 map 的闭包只在调用 forEach 后才会计算
+		 */
+		factor[0] = 0;
+		stream.forEach(System.out::println);
+	}
+}
---
+++ src/main/java/pers/allen/explore/feature/java8/stream/ParallelStreamTest.java
@@ -0,0 +1,43 @@
+package pers.allen.explore.feature.java8.stream;
+
+import java.time.Duration;
+import java.time.LocalDateTime;
+import java.util.Arrays;
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+/**
+ * 并行流（使用ForkJoinPool中的commonPool）
+ * @author lengyul
+ * @date 2019年5月29日 上午11:08:46
+ */
+public class ParallelStreamTest {
+
+	
+	public static void main(String[] args) {
+		List<Integer> numbers = Arrays.asList(1,2,3,4,5,6,7,8);
+		
+		LocalDateTime start = LocalDateTime.now();
+		
+		// 使用并行流在 8 核心处理器所用时间约为 1 秒，因为在默认情况下，并行流使用了与系统上的核心数一样多的线程
+		numbers.stream().parallel()
+			.map(ParallelStreamTest::simulateTimeComputation)
+			.forEachOrdered(System.out::println);
+		
+		LocalDateTime end = LocalDateTime.now();
+		System.out.println("Seconds：" + Duration.between(start, end).getSeconds());
+	}
+	
+	/*
+	 * 延时操作
+	 */
+	private static int simulateTimeComputation(int number) {
+		try {
+			TimeUnit.SECONDS.sleep(1);
+		} catch (InterruptedException e) {
+			e.printStackTrace();
+		}
+		return number * 2;
+	}
+	
+}
