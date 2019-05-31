2c16c7e33136d02bc3405b9839329d5a4ab2c166
lengyul <1174959274@qq.com>
2019-05-31 09:57:32 +0800

    function

--- src/main/java/pers/allen/explore/feature/java8/functions/CalcFunction.java
+++ src/main/java/pers/allen/explore/feature/java8/functions/CalcFunction.java
@@ -1,5 +1,11 @@
 package pers.allen.explore.feature.java8.functions;
 
+/*
+ * 函数接口规则：
+ * 一个函数接口只有一个抽象方法
+ * 在 Object 类中属于公共方法的抽象方法不会被视为单一抽象方法
+ * 函数接口可以有默认方法和静态方法
+ */
 @FunctionalInterface
 public interface CalcFunction {
 	
