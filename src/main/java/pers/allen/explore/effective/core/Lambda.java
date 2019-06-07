6ac5fa7129050a223d0fcda909ddd9cdda3502f6
lengyul <1174959274@qq.com>
2019-06-07 10:42:57 +0800

    effective [lambda and stream]

---
+++ src/main/java/pers/allen/explore/effective/core/Lambda.java
@@ -0,0 +1,86 @@
+package pers.allen.explore.effective.core;
+
+import java.util.Comparator;
+import java.util.List;
+
+
+/**
+ * Lambda 优先于匿名类
+ * @author lengyul
+ * @date 2019年6月5日 下午7:43:12
+ */
+public class Lambda {
+
+	
+	/*
+	 * 在以往对接口创建实例的主要方式是通过 匿名类（anonymous class）
+	 */
+	private List<String> list = null;
+	// 对 list 进行排序
+	public void test() {
+		list.sort(new Comparator<String>() {
+			@Override
+			public int compare(String o1, String o2) {
+				return Integer.compare(o1.length(), o2.length());
+			}
+		});
+	}
+	
+	/*
+	 * 	在 Java 8 中，出现了 带有单个抽象方法的接口，这些接口现在被称作函数接口（functional interface），
+	 * Java 允许利用 Lambda 表达式（Lambda expression）创建这些接口的实例
+	 * Lambda 类似于匿名类的函数（语法糖），但是比它简洁的多
+	 */
+	public void test2() {
+		list.sort((o1,o2) -> Integer.compare(o1.length(), o2.length()));
+	}
+	
+	/*
+	 * 简短的 Lambda 表达式能提高代码可读性，这是函数式编程的重要好处之一
+	 * 
+	 * tips：
+	 * Lambda 没有名称和文档；如果一个计算本身不是自描述的，或者超出了几行，就不要把它放在一个 Lambda 中
+	 * 对于 Lambda 而言，一行是最理想的，三行是合理的最大极限；包含多行的 Lambda 表达式会让代码变得杂乱且难以阅读
+	 * 
+	 * Lambda 中无法获得对自身的引用（this），Lambda 中引用的局部变量必须是声明为 final类型
+	 */
+	
+	/**
+	 * 方法引用优先于 Lambda
+	 * @author lengyul
+	 * @date 2019年6月5日 下午8:06:35
+	 */
+	private class MethodReference {
+		/*
+		 * Java 提供了生成比 Lambda 更简洁函数对象的方法：方法引用
+		 * 对于 Lambda 中的传递一个或者多个形参，最好将它替换为方法引用，使用 :: 来调用 ，无论是传递给静态方法还是实例方法
+		 * 
+		 */
+		public void test() {
+			list.sort(Comparator.comparingInt(String::length)); // str.length()
+		}
+		
+		public void test2() { // list中的每个元素大写
+			list.stream().map((e) -> e.toUpperCase()); // lambda
+			list.stream().map(String::toUpperCase); // 方法引用
+		}
+		
+		/**
+		 * 
+		 * 五种方法引用
+		 * ______________________________________________________________________
+		 *|	 方法引用类型			范例			 			Lambda 等式				 |
+		 *|   静态			Integer::parseInt	 	str -> Integer.parseInt(str) |
+		 *|   有限制			Instant.now()::isAfter	Instant then = Instant.now();t -> then.isAfter();
+		 *    无限制			String::toLowerCase		str -> str.toLowerCase()
+		 *|	    类构造器		String::new				() -> new String()			 |	
+		 *|   数组构造器 		int[] ::new 			len -> new int[len]			 |
+		 *|______________________________________________________________________|	 
+		 */
+		
+		/*
+		 * 只要方法引用更加简洁，清晰，就用方法引用；如果方法引用并不简洁，就坚持使用 Lambda
+		 */
+	}
+	
+}
---
+++ src/main/java/pers/allen/explore/effective/core/Stream.java
@@ -0,0 +1,66 @@
+package pers.allen.explore.effective.core;
+
+import org.junit.Test;
+
+/**
+ * Stream API 
+ * @author lengyul
+ * @date 2019年6月5日 下午8:32:12
+ */
+public class Stream {
+
+	/*
+	 * 	在Java 8 中增加了 Stream API，简化了串行或并行的大批量操作
+	 * Stream（流）代表数据元素有限或无限的顺序，Stream pipeline（流管道）则代表这些元素的一个多级计算
+	 * 
+	 * Stream 中的元素可能来自任何位置，常见的来源包括集合、数组、文件、伪随机数生成器以及其他的 Stream
+	 * Stream 中的数据元素可以是对象引用、或者基本数据类型，它支持三种基本类型：int、long 和 double
+	 *  
+	 */
+	
+	/*
+	 * 
+	 * 	一个 Stream pipeline 中包含一个源 Stream，接着是 0个或者多个中间操作（intermediate operation）
+	 * 和一个终止操作（terminal operation）
+	 * 中间操作：对 Stream 中的元素进行转换、改变、过滤等
+	 * 终止操作：会在最后一个中间操作产生的 Stream 上执行一个最终的计算，例如：返回集合、某个元素或者打印出所有元素等
+	 * @see StreamTest
+	 * 
+	 * Stream pipeline 通常是 lazy 的：直到调用终止操作时才会开始计算（没有终止操作的 Stream 不会执行中间操作）
+	 * @see LazyCalculation
+	 * 
+	 * Stream API 是流式（fluent）的：所有包含 pipeline 的调用可以链接成一个表达式
+	 * @see CollectionStream 	list.stream().filter().map();
+	 */
+	
+	
+	/*
+	 * 避免利用 Stream 来处理 char 值
+	 */
+	@Test
+	public void test() {
+		// chars() 返回的 Stream 中的元素，并不是 char 值，而是 int 值
+		"Hello world".chars().forEach(System.out::print);
+		 System.out.println();
+		"Hello world".chars().forEach(x -> System.out.print((char) x));
+	}
+	
+	/*
+	 * 使用 Stream 的一些优势
+	 * 可以使用并行流对元素进行操作
+	 * 统一转换、过滤元素的序列
+	 * 计算元素、合并元素的顺序
+	 * 对元素进行排序、收集、分组
+	 * 
+	 * -----------------------------------------------------------
+	 * 
+	 * 使用 Stream 不能完成的一些工作
+	 * Lambda 中不能修改任何 local 变量
+	 * forEach 中无法获取当前执行的位置（i），不能使用 return、break、continue 或者抛出受检异常
+	 * 
+	 * 对于 Stream forEach 和 迭代，具体选择哪一种方法，并没有硬性、速成的规则，如果不确定，
+	 * 那么就两种都试试，看一看哪种更好用
+	 */
+	
+	
+}
