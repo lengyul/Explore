7270f40fad844aa00efcfbe977d1cd922bba9038
lengyul <1174959274@qq.com>
2019-07-01 10:37:51 +0800

    test[static load]

--- src/main/java/pers/allen/explore/test/InitialTest.java
+++ src/main/java/pers/allen/explore/test/InitialTest.java
@@ -15,5 +15,10 @@ public class InitialTest {
 		private static final Boolean LIVING = true;
 		private final Boolean alive = LIVING;
 		// alive 字段依赖于 LIVING 字段 ，但 LIVING 字段 在 LIFE 字段初始化之后，导致 alive 字段初始化失败
+		/*
+		 *  解决办法：
+		 *  	 1. LIVING 字段 放在 LIFE字段 实例化之前 （类变量根据顺序执行）
+		 *  	 2. LIVING 字段 Boolean 改为 boolean （boolean设置默认值为true）
+		 */
 	}	
 }
